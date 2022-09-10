80403af965da386fb86bcdc08e8d16a7a5bc48f9
gsantchoukenfack@gmail.com <gsantchoukenfack@gmail.com>
2022-09-10 10:31:31 +0100

    add / to build in server.js file

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use("/auth/users", userRoutes);
 
 //.....................DEPLOYMENT..................................
 
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "/build")));
 
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
